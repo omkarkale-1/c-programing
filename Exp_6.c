a3d6fbdbf3a7fa617584ce4623a4428d4467c8eb
omkarkale-1 <omkarkale2906@gmail.com>
2025-11-09 23:28:03 +0530

    Create Exp_6.c

---
+++ Exp_6.c
@@ -0,0 +1,22 @@
+// Name :- Omkar Kale
+// Class : Computer Enginnering
+// Division :- D
+// UIN :- 251P134
+
+#include <stdio.h>
+
+void Counter()
+{
+    static int count = 0;
+    count++;
+    printf("Count = %d\n", count);
+}
+
+int main()
+{
+    Counter();
+    Counter();
+    Counter();
+
+    return 0;
+}
